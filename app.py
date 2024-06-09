0dec26c4f5f218bf76a1d5218bda7dfde419af74
g0urav-hustler <gouravchouhan966@gmail.com>
2024-06-09 17:28:25 +0530

    add exceptional handler

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import time
-import urllib
+import urllib.request
 import json
 
 st.set_page_config(page_title="Blog Generation App",layout="wide")
@@ -25,33 +25,32 @@ def main():
     blog_topic = st.text_input("", placeholder= "Write your Blog topic here")
 
     if blog_topic:
-        st.subheader("Generated Blog")
-        # generated_blog = """Data science is the study of data to extract meaningful insights for business. It is a multidisciplinary approach that combines principles and practices from the fields of mathematics, statistics, artificial intelligence, and computer engineering to analyze large amounts of data. This analysis helps data scientists to ask and answer questions like what happened, why it happened, what will happen, and what can be done with the results.
-        # Data science is important because it combines tools, methods, and technology to generate meaning from data. Modern organizations are inundated with data; there is a proliferation of devices that can automatically collect and store information. Online systems and payment portals capture more data in the fields of e-commerce, medicine, finance, and every other aspect of human life. We have text, audio, video, and image data available in vast quantities."""
         
-        # generated_blog = """Transformer is a neural network architecture used for performing machine learning tasks. In 2017 Vaswani et al. published a paper ” Attention is All You Need” in which the transformers architecture was introduced. Since then, transformers have been widely adopted and extended for various machine learning tasks beyond NLP. The article explores the architecture, working and applications of transformer.
-        # Transformer Architecture is a model that uses self-attention that transforms one whole sentence into a single sentence. This is a big shift from how older models work step by step, and it helps overcome the challenges seen in models like RNNs and LSTMs."""
-        with st.spinner("Generating the blog 🪄"):
-            data = {
-                "blog_topic": str(blog_topic)
-            }
+            try:   
+                st.subheader("Generated Blog")
+                with st.spinner("Generating the blog 🪄"):
+                    data = {
+                        "blog_topic": str(blog_topic)
+                    }
 
+                    data = json.dumps(data)
+                    data = str(data)
+                    data = data.encode("utf-8")
+                    
+                    response = urllib.request.urlopen(url = url, data = data)
+
+                    model_response = response.read()
+
+                    model_response = json.loads(model_response)
+
+                    generated_blog = model_response['body']
+
+                st.write_stream(response_generator(generated_blog))
             
-            data = json.dumps(data)
-            data = str(data)
-            data = data.encode("utf-8")
+            except Exception as e:
 
-            response = urllib.request.urlopen(url = url, data = data)
+                st.warning("App is under maintenance due to service charges.")
 
-            model_response = response.read()
-
-            model_response = json.loads(model_response)
-
-            generated_blog = model_response['body']
-            
-            time.sleep(2)
-
-        st.write_stream(response_generator(generated_blog))
 
 if __name__ == "__main__":
     main()

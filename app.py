c83e9080fb154a9426f46a8f9ef2db6f1f44921f
g0urav-hustler <gouravchouhan966@gmail.com>
2024-06-09 14:26:18 +0530

    add subheader

--- app.py
+++ app.py
@@ -1,29 +1,35 @@
 import streamlit as st
 import time
 
-st.set_page_config(layout="wide")
+st.set_page_config(page_title="Blog Generation App",layout="wide")
 
 
 # Streamed response emulator
 def response_generator(response):
     for letter in response:
         yield letter
-        time.sleep(0.04)
+        time.sleep(0.02)
 
 
 def main():
-    st.title("Blog Generation")
+    st.title("Blog Generation App")
 
-    st.subheader("Blog Title")
+    st.subheader("This app helps to generate blog on favorate topic that you want to write 🖋📃")
+    st.divider()
+
+    st.subheader("Blog Topic")
 
     blog_topic = st.text_input("", placeholder= "Write your Blog topic here")
 
     if blog_topic:
         st.subheader("Generated Blog")
-        generated_blog = "Language is the basis for human interaction and communication. Speaking and listening are the direct by-products of human reliance on language. While humans can use language to understand each other, in today’s digital world, they must also interact with machines."
-
+        generated_blog = """Data science is the study of data to extract meaningful insights for business. It is a multidisciplinary approach that combines principles and practices from the fields of mathematics, statistics, artificial intelligence, and computer engineering to analyze large amounts of data. This analysis helps data scientists to ask and answer questions like what happened, why it happened, what will happen, and what can be done with the results.
+        Data science is important because it combines tools, methods, and technology to generate meaning from data. Modern organizations are inundated with data; there is a proliferation of devices that can automatically collect and store information. Online systems and payment portals capture more data in the fields of e-commerce, medicine, finance, and every other aspect of human life. We have text, audio, video, and image data available in vast quantities."""
+        
+        with st.spinner("Generating the blog 🪄"):
+            time.sleep(2)
+            
         st.write_stream(response_generator(generated_blog))
-
 
 if __name__ == "__main__":
     main()
--- lambda_function.py
+++ lambda_function.py
@@ -15,24 +15,31 @@ def generate_blog(blog_topic : str) -> str:
         "top_p": 0.9
     }
 
-    bedrock = boto3.client("bedrock-runtime", region_name = "us-east-1",
-                           config = botocore.config.Config(read_timeout = 300, retries = {"max_attemps":3})
-                          )
-    response = bedrock.invoke_model(modelId = Model_Name , body = json.dumps(body))
-    response_content=response.get('body').read()
-    response_data=json.loads(response_content)
-        
-    blog_details=response_data['generation']
-    return blog_details
+    try:
+        bedrock=boto3.client("bedrock-runtime",region_name="us-east-1",
+                             config=botocore.config.Config(read_timeout=300,retries={'max_attempts':3}))
+        response=bedrock.invoke_model(body=json.dumps(body),modelId="meta.llama2-13b-chat-v1")
+
+        response_content=response.get('body').read()
+        response_data=json.loads(response_content)
+        blog_details=response_data['generation']
+        return blog_details
+    except Exception as e:
+        print(f"Error generating the blog:{e}")
+        return ""
 
 def lambda_handler(event,context):
 
     event = json.loads(event)
     blog_topic = event["blog_topic"]
 
-    blog = generate_blog(blog_topic= blog_topic)
 
+    blog_topic = event["blog_topic"]
+    
+    generated_blog = generate_blog(blog_topic)
+    
+    
     return {
-        "statusCode": 200,
-        "body": json.dumps(blog)
+        'statusCode': 200,
+        'body': json.dumps(generated_blog)
     }

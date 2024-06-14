5dbdcd79e41a829b12c1f5efb45e0b5b0a979894
g0urav-hustler <gouravchouhan966@gmail.com>
2024-06-14 18:16:17 +0530

    adding exception handelling

--- lambda_function.py
+++ lambda_function.py
@@ -1,45 +1,48 @@
-import boto3
-import botocore.config
 import json
+import boto3
 
 Model_Name = "meta.llama2-13b-chat-v1"
 
 def generate_blog(blog_topic : str) -> str:
-    prompt=f"""<s>[INST]Write a 20 words blog on the topic {blogtopic}[/INST]
-    """
-
-    body = {
-        "promt": prompt,
+    
+    prompt = f"write a 100 word blog on {blog_topic} and start with the word {blog_topic}"
+    
+    body = json.dumps({
+        "prompt": prompt,
         "max_gen_len": 512,
         "temperature": 0.5,
         "top_p": 0.9
-    }
-
+    })
+    
+    
     try:
-        bedrock=boto3.client("bedrock-runtime",region_name="us-east-1",
-                             config=botocore.config.Config(read_timeout=300,retries={'max_attempts':3}))
-        response=bedrock.invoke_model(body=json.dumps(body),modelId="meta.llama2-13b-chat-v1")
-
+        bedrock_runtime = boto3.client(
+                service_name='bedrock-runtime',
+                region_name='us-east-1'
+            )
+        
+        response=bedrock_runtime.invoke_model(body=body,modelId="meta.llama2-13b-chat-v1")
+        
         response_content=response.get('body').read()
         response_data=json.loads(response_content)
+        
         blog_details=response_data['generation']
         return blog_details
     except Exception as e:
         print(f"Error generating the blog:{e}")
         return ""
-
-def lambda_handler(event,context):
-
-    event = json.loads(event)
-    blog_topic = event["blog_topic"]
-
-
+    
+    
+    
+def lambda_handler(event, context):
+    
+    # extract the blog topic
     blog_topic = event["blog_topic"]
     
+    # get generated blog
     generated_blog = generate_blog(blog_topic)
-    
-    
+
     return {
         'statusCode': 200,
-        'body': json.dumps(generated_blog)
+        'generated_blog': generated_blog
     }
